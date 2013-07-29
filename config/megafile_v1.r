c120be9f4bbb06e0ee4ca8cd805c9b9e7fb9a429
Rafael Ramos <rrasmo@gmail.com>
2013-07-29 11:36:51 +0100

    Renamed columns

--- config/megafile_v1.r
+++ config/megafile_v1.r
@@ -5,8 +5,7 @@ config = list(
 		"amee_id",
 		"est_mtco2e",
 		"score_d",
-		"uk_modsal",
-		"au_modsal"
+		"turnover"
 	),
 	
 	# Rename columns to standard names
@@ -14,13 +13,10 @@ config = list(
 		est_mtco2e="emissions_total",
 		score_d="amee_industry_score",
 		amee_id="amee_company_id",
-		uk_modsal="annual_sales_local"
+		turnover="annual_sales_local"
 	),
 
 	# Execute raw code
 	transform_data = list(
-		# If uk_modsal is missing, then use the value from au_modsal
-		'missing_sales_indexes = (companies$annual_sales_local==0 | is.na(companies$annual_sales_local))',
-		'companies$annual_sales_local[missing_sales_indexes] = companies$au_modsal[missing_sales_indexes]'
 	)
 )
